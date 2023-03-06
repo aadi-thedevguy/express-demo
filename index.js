63fe2df86a262695e6066b75e220932dc7411094
aadi-thedeyguy <adikhare24@gmail.com>
2023-03-06 13:34:33 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require("express")
+const express = require('express');
 
-const app = express()
+const app = express();
+app.use(express.urlencoded({extended: false}))
 
-app.use((req,res, next) => {
-    console.log('In the middleware')
-    next()
-})
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+});
 
-app.use((req,res, next) => {
-    console.log('In another middleware')
-    res.send('<h1>Hello from Express</h1>')
-})
+app.post('/product', (req, res, next) => {
+    const {title, size} = req.body
+    console.log(title,size);
+    res.redirect('/');
+});
 
-app.listen(3000)
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
