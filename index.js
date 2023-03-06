2eb3989afc39739542cfcd7f2f74e712a0a3fe8a
aadi-thedeyguy <adikhare24@gmail.com>
2023-03-06 13:41:46 +0530

    second commit

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 app.use(express.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    const {title, size} = req.body
-    console.log(title,size);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
