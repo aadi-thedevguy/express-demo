e50d4730ba3d98d0691231ff7b7894b03695ac5e
aadi-thedeyguy <adikhare24@gmail.com>
2023-03-07 23:26:27 +0530

    added sql queries

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const errorController = require('./controllers/error');
-const db = require('./util/database')
+const db = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,7 +14,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
