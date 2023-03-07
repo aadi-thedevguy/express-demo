ee4b10bc0dbfdb1a253b48b25afbd81d463f325a
aadi-thedeyguy <adikhare24@gmail.com>
2023-03-07 23:46:25 +0530

    added sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host : 'localhost',
-    user: 'root',
-    database : 'express-demo',
-    password : 'Aditya@24'
-})
+const sequelize = new Sequelize('express-demo', 'root', 'Aditya@24', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-module.exports = pool.promise()
+module.exports = sequelize;
